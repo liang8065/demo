7230422d0c3cf34cbfcc253c42feabb3573e7234
quanliangliang-pd <quanliangliang-pd@ds04.se.shyc2.qihoo.net>
2018-11-19 20:31:33 +0800

    add log to train_unk.sh

--- LM/sh/train_unk.sh
+++ LM/sh/train_unk.sh
@@ -1,3 +1,10 @@
+export PS4='+{$LINENO($(date +"%F %T")):${FUNCNAME[0]}} '
+full_path=$(readlink -f $0)
+cd $(dirname $full_path)
+exec 1>${full_path%.*}.log
+exec 2>${full_path%.*}.err
+set -x
+
 lmplz -o 5 --skip_symbols "<unk>" < ../data/ptb.train.txt.UNK > ../model/5gram.arpa.unk
 build_binary ../model/5gram.arpa.unk ../model/5gram.binary.unk
 
@@ -10,7 +17,4 @@ build_binary ../model/3gram.arpa.unk ../model/3gram.binary.unk
 lmplz -o 2 --skip_symbols "<unk>" < ../data/ptb.train.txt.UNK > ../model/2gram.arpa.unk
 build_binary ../model/2gram.arpa.unk ../model/2gram.binary.unk
 
-
-
-
-
+set +x
